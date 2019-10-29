6f50b5f17432aacaa1e9d92ec123618941f37136
chokomancarr <puakai2010@gmail.com>
2019-10-29 13:08:10 +0900

    add return checks, fk macos actually

--- src/vulkanapi.cpp
+++ src/vulkanapi.cpp
@@ -2,6 +2,15 @@
 #include <iostream>
 #include <algorithm>
 #include <set>
+#include <vector>
+
+VkResult result;
+
+#define VKDO(cmd) if ((result = cmd) != VK_SUCCESS) {\
+    std::cerr << #cmd << " failed with error code " << (int)result << std::endl;\
+    abort();\
+}
+#define FNCOK std::cout << __func__ << "() ok" << std::endl;
 
 GLFWwindow* VulkanAPI::window;
 
@@ -33,48 +42,48 @@ void VulkanAPI::Init() {
     appinfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
     appinfo.apiVersion = VK_API_VERSION_1_0;
 
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
     VkInstanceCreateInfo createinfo = {};
     createinfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     createinfo.pApplicationInfo = &appinfo;
-
-    uint32_t glfwExtensionCount = 0;
-    const char** glfwExtensions;
-
-    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-
     createinfo.enabledExtensionCount = glfwExtensionCount;
     createinfo.ppEnabledExtensionNames = glfwExtensions;
     createinfo.enabledLayerCount = 0;
 
-    if (vkCreateInstance(&createinfo, 0, &instance) != VK_SUCCESS)
-        exit(-1);
+    VKDO(vkCreateInstance(&createinfo, 0, &instance));
     
+    FNCOK
 }
 
 void VulkanAPI::InitDevice() {
     uint32_t count = 0;
-    vkEnumeratePhysicalDevices(instance, &count, nullptr);
+    VKDO(vkEnumeratePhysicalDevices(instance, &count, nullptr));
     std::cout << "available physical devices: " << count << std::endl;
+    if (!count) exit(0);
     std::vector<VkPhysicalDevice> devices(count);
-    vkEnumeratePhysicalDevices(instance, &count, devices.data());
+    VKDO(vkEnumeratePhysicalDevices(instance, &count, devices.data()));
+    
     physDevice = devices[0];
 
     vkGetPhysicalDeviceQueueFamilyProperties(physDevice, &count, nullptr);
-    std::vector<VkQueueFamilyProperties> props(count);
-    vkGetPhysicalDeviceQueueFamilyProperties(physDevice, &count, props.data());
-    uint32_t prop;
+    std::vector<VkQueueFamilyProperties> queueFamilies(count);
+    vkGetPhysicalDeviceQueueFamilyProperties(physDevice, &count, queueFamilies.data());
+    uint32_t graphicsFamily = 0;
     for (uint32_t a = 0; a < count; a++) {
         VkBool32 pres;
-        vkGetPhysicalDeviceSurfaceSupportKHR(physDevice, a, surface, &pres);
-        if (pres && (props[a].queueFlags & VK_QUEUE_GRAPHICS_BIT) > 0) {
-            prop = a;
+        VKDO(vkGetPhysicalDeviceSurfaceSupportKHR(physDevice, a, surface, &pres));
+        if (pres && (queueFamilies[a].queueFlags & VK_QUEUE_GRAPHICS_BIT) > 0) {
+            graphicsFamily = a;
             break;
         }
     }
 
     VkDeviceQueueCreateInfo queueCreateInfo = {};
     queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-    queueCreateInfo.queueFamilyIndex = prop;
+    queueCreateInfo.queueFamilyIndex = graphicsFamily;
     queueCreateInfo.queueCount = 1;
     float queuePriority = 1.0f;
     queueCreateInfo.pQueuePriorities = &queuePriority;
@@ -87,18 +96,21 @@ void VulkanAPI::InitDevice() {
     createInfo.queueCreateInfoCount = 1;
     createInfo.pEnabledFeatures = &deviceFeatures;
 
-    if(vkCreateDevice(physDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
-        exit(-3);
-    }
+    VKDO(vkCreateDevice(physDevice, &createInfo, nullptr, &device));
 
-    vkGetDeviceQueue(device, prop, 0, &graphicsQueue);
-    vkGetDeviceQueue(device, prop, 0, &presentQueue);
+    vkGetDeviceQueue(device, graphicsFamily, 0, &graphicsQueue);
+    presentQueue = graphicsQueue;
+
+
+    FNCOK
 }
 
 void VulkanAPI::CreateSurface() {
-    if (glfwCreateWindowSurface(instance, window, nullptr, &surface) != VK_SUCCESS) {
-        exit(-2);
-    }
+    if (!glfwVulkanSupported()) abort();
+
+    VKDO(glfwCreateWindowSurface(instance, window, nullptr, &surface));
+
+    FNCOK
 }
 
 void VulkanAPI::DestroySurface() {
@@ -106,5 +118,7 @@ void VulkanAPI::DestroySurface() {
 }
 
 void VulkanAPI::Exit() {
+    vkDestroyDevice(device, nullptr);
+
     vkDestroyInstance(instance, nullptr);
 }
--- src/vulkanapi.hpp
+++ src/vulkanapi.hpp
@@ -1,4 +1,3 @@
-#include <vulkan/vulkan.hpp>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
